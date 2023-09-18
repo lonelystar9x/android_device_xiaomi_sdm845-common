3e4618088b74c73322fce60de4d82416f68126d8
Peter Kalauskas <peskal@google.com>
2023-09-18 22:10:57 +0000

    sdm845-common: parts: Enable use_resource_processor for all sysui deps
    This will make the build faster
    
    Test: m checkbuild
    Bug: 295208392
    Change-Id: I0c1bd901429bbe3bf81c1530e156735f8637a96e

--- parts/src/org/lineageos/settings/soundcontrol/SoundControlSettingsActivity.java
+++ parts/src/org/lineageos/settings/soundcontrol/SoundControlSettingsActivity.java
@@ -21,7 +21,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class SoundControlSettingsActivity extends CollapsingToolbarBaseActivity {
 
@@ -31,7 +30,7 @@ public class SoundControlSettingsActivity extends CollapsingToolbarBaseActivity 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction()
-            .replace(R.id.content_frame,  new SoundControlSettings())
+            .replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,  new SoundControlSettings())
             .commit();
     }
 
