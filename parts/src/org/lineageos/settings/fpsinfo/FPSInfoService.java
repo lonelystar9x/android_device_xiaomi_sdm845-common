XtremeOrnob/xiaomi_sdm845-common

[SQUASH] sdm845-common: parts: introduce FPS counter QS Tile
Squash of the following:

sdm845-common: parts: introduce FPS overlay

NurKeinNeid edit: adapted to msm8998
idoybh edit: measure correct text length
nullxception edit: adapt to sdm845-common parts and only pick the service
since it'll be handled by idoybh's QS Tile approach

sdm845-common: parts: adapt FPS Info based on exposed data from kernel

* Current kernel emits measured_fps inside /sys/class/drm/card0/sde-crtc-0.
  It also has a different output format than the original patches.
  So adapt it so measured fps can be displayed properly.

* Shift position by 50px more so the fps counter is 100%
  visible without getting cutted by the hardware corner.

* Also, cleanup some unused imports and redundant file reader since
  we're already has that inside utils/.

sdm845-common: parts: Add FPS overlay QS tile

sdm845-common: parts: Digital-style icon for FPSInfo QS Tile

sdm845-common: parts: FPSInfoService: Switch to isDreaming check

The isDozing check was dependend on this commit: https://review.aosip.dev/c/AOSIP/platform_frameworks_base/+/10485
The commit isn't needed on Android 10 (read comments) and switching to the regular AOSP check makes more sense and works the exact same.

sdm845-common: sepolicy: Address denials for FPS QS tile

Co-authored-by: Marko Man <darkobas@gmail.com>
Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
Co-authored-by: SonalSingh18 <sonal.singh.19993@gmail.com>
Change-Id: I50fd2ea149909b03ee6a4c32f53f990a01b929e5
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

parts/src/org/lineageos/settings/fpsinfo/FPSInfoService.java
@@ -1,0 +1,288 @@
+/*
+ * Copyright (C) 2019 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.fpsinfo;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Typeface;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.service.dreams.DreamService;
+import android.service.dreams.IDreamManager;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+
+import java.lang.Math;
+import java.lang.StringBuffer;
+
+import org.lineageos.settings.utils.FileUtils;;
+
+public class FPSInfoService extends Service {
+    private View mView;
+    private Thread mCurFPSThread;
+    private final String TAG = "FPSInfoService";
+    private String mFps = null;
+
+    private static final String MEASURED_FPS = "/sys/class/drm/sde-crtc-0/measured_fps";
+
+    private IDreamManager mDreamManager;
+
+    private class FPSView extends View {
+        private Paint mOnlinePaint;
+        private float mAscent;
+        private int mFH;
+        private int mMaxWidth = 0;
+
+        private int mNeededWidth;
+        private int mNeededHeight;
+
+        private boolean mDataAvail;
+
+        private Handler mCurFPSHandler = new Handler() {
+            public void handleMessage(Message msg) {
+                if(msg.obj == null || msg.what != 1) {
+                    return;
+                }
+
+                String msgData = (String) msg.obj;
+                msgData = msgData.trim().split("\\s+")[1];
+                mFps = msgData + " FPS";
+                mDataAvail = true;
+                updateDisplay();
+            }
+        };
+
+        FPSView(Context c) {
+            super(c);
+            float density = c.getResources().getDisplayMetrics().density;
+            int paddingPx = Math.round(5 * density);
+            setPadding(paddingPx, paddingPx, paddingPx, paddingPx);
+            setBackgroundColor(Color.argb(0x60, 0, 0, 0));
+
+            final int textSize = Math.round(12 * density);
+
+            Typeface typeface = Typeface.create("monospace", Typeface.NORMAL);
+
+            mOnlinePaint = new Paint();
+            mOnlinePaint.setTypeface(typeface);
+            mOnlinePaint.setAntiAlias(true);
+            mOnlinePaint.setTextSize(textSize);
+            mOnlinePaint.setColor(Color.WHITE);
+            mOnlinePaint.setShadowLayer(5.0f, 0.0f, 0.0f, Color.BLACK);
+
+            mAscent = mOnlinePaint.ascent();
+            float descent = mOnlinePaint.descent();
+            mFH = (int)(descent - mAscent + .5f);
+
+            updateDisplay();
+        }
+
+        @Override
+        protected void onAttachedToWindow() {
+            super.onAttachedToWindow();
+        }
+
+        @Override
+        protected void onDetachedFromWindow() {
+            super.onDetachedFromWindow();
+            mCurFPSHandler.removeMessages(1);
+        }
+
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            setMeasuredDimension(resolveSize(mNeededWidth, widthMeasureSpec),
+                    resolveSize(mNeededHeight, heightMeasureSpec));
+        }
+
+        private String getFPSInfoString() {
+            return mFps;
+        }
+
+        @Override
+        public void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            if (!mDataAvail) {
+                return;
+            }
+
+            final int W = mNeededWidth;
+            final int LEFT = getWidth()-1;
+
+            int x = LEFT - mPaddingLeft;
+            int top = mPaddingTop + 2;
+            int bottom = mPaddingTop + mFH - 2;
+
+            int y = mPaddingTop - (int)mAscent;
+
+            String s=getFPSInfoString();
+            canvas.drawText(s, LEFT-mPaddingLeft-mMaxWidth,
+                    y-1, mOnlinePaint);
+            y += mFH;
+        }
+
+        void updateDisplay() {
+            if (!mDataAvail) {
+                return;
+            }
+
+            if (mOnlinePaint != null) {
+                mMaxWidth = (int) mOnlinePaint.measureText(mFps);
+            }
+
+            int neededWidth = mPaddingLeft + mPaddingRight + mMaxWidth;
+            int neededHeight = mPaddingTop + mPaddingBottom + 40;
+            if (neededWidth != mNeededWidth || neededHeight != mNeededHeight) {
+                mNeededWidth = neededWidth;
+                mNeededHeight = neededHeight;
+                requestLayout();
+            } else {
+                invalidate();
+            }
+        }
+
+        public Handler getHandler(){
+            return mCurFPSHandler;
+        }
+    }
+
+    protected class CurFPSThread extends Thread {
+        private boolean mInterrupt = false;
+        private Handler mHandler;
+
+        public CurFPSThread(Handler handler){
+            mHandler=handler;
+        }
+
+        public void interrupt() {
+            mInterrupt = true;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (!mInterrupt) {
+                    sleep(1000);
+                    StringBuffer sb=new StringBuffer();
+                    String fpsVal = FileUtils.readOneLine(MEASURED_FPS);
+                    mHandler.sendMessage(mHandler.obtainMessage(1, fpsVal));
+                }
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mView = new FPSView(this);
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.TYPE_SECURE_SYSTEM_OVERLAY,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+            PixelFormat.TRANSLUCENT);
+        params.y = 50;
+        params.gravity = Gravity.LEFT | Gravity.TOP;
+        params.setTitle("FPS Info");
+
+        startThread();
+
+        mDreamManager = IDreamManager.Stub.asInterface(
+                ServiceManager.checkService(DreamService.DREAM_SERVICE));
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+
+        WindowManager wm = (WindowManager)getSystemService(WINDOW_SERVICE);
+        wm.addView(mView, params);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopThread();
+        ((WindowManager)getSystemService(WINDOW_SERVICE)).removeView(mView);
+        mView = null;
+        unregisterReceiver(mScreenStateReceiver);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                Log.d(TAG, "ACTION_SCREEN_ON " + isDozeMode());
+                if (!isDozeMode()) {
+                    startThread();
+                    mView.setVisibility(View.VISIBLE);
+                }
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                Log.d(TAG, "ACTION_SCREEN_OFF");
+                mView.setVisibility(View.GONE);
+                stopThread();
+            }
+        }
+    };
+
+    private boolean isDozeMode() {
+        try {
+            if (mDreamManager != null && mDreamManager.isDreaming()) {
+                return true;
+            }
+        } catch (RemoteException e) {
+            return false;
+        }
+        return false;
+    }
+
+    private void startThread() {
+        Log.d(TAG, "started CurFPSThread");
+        mCurFPSThread = new CurFPSThread(mView.getHandler());
+        mCurFPSThread.start();
+    }
+
+    private void stopThread() {
+        if (mCurFPSThread != null && mCurFPSThread.isAlive()) {
+            Log.d(TAG, "stopping CurFPSThread");
+            mCurFPSThread.interrupt();
+            try {
+                mCurFPSThread.join();
+            } catch (InterruptedException e) {
+            }
+        }
+        mCurFPSThread = null;
+    }
+}

lonelystar9x/android_device_xiaomi_sdm845-common

parts/src/org/lineageos/settings/fpsinfo/FPSInfoService.java
@@ -1,0 +1,287 @@
+/*
+ * Copyright (C) 2019 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.fpsinfo;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Typeface;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.service.dreams.DreamService;
+import android.service.dreams.IDreamManager;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+
+import java.lang.Math;
+import java.lang.StringBuffer;
+
+import org.lineageos.settings.utils.FileUtils;;
+
+public class FPSInfoService extends Service {
+    private View mView;
+    private Thread mCurFPSThread;
+    private final String TAG = "FPSInfoService";
+    private String mFps = null;
+
+    private static final String MEASURED_FPS = "/sys/class/drm/card0/sde-crtc-0/measured_fps";
+
+    private IDreamManager mDreamManager;
+
+    private class FPSView extends View {
+        private Paint mOnlinePaint;
+        private float mAscent;
+        private int mFH;
+        private int mMaxWidth = 0;
+
+        private int mNeededWidth;
+        private int mNeededHeight;
+
+        private boolean mDataAvail;
+
+        private Handler mCurFPSHandler = new Handler() {
+            public void handleMessage(Message msg) {
+                if(msg.obj == null || msg.what != 1) {
+                    return;
+                }
+                String msgData = (String) msg.obj;
+                msgData = msgData.trim().split("\\s+")[1];
+                mFps = msgData + " FPS";
+                mDataAvail = true;
+                updateDisplay();
+            }
+        };
+
+        FPSView(Context c) {
+            super(c);
+            float density = c.getResources().getDisplayMetrics().density;
+            int paddingPx = Math.round(9 * density);
+            setPadding(paddingPx, paddingPx, paddingPx, paddingPx);
+            setBackgroundColor(Color.argb(0x0, 0, 0, 0));
+
+            final int textSize = Math.round(16 * density);
+
+            Typeface typeface = Typeface.create("googlesans", Typeface.BOLD);
+
+            mOnlinePaint = new Paint();
+            mOnlinePaint.setTypeface(typeface);
+            mOnlinePaint.setAntiAlias(true);
+            mOnlinePaint.setTextSize(textSize);
+            mOnlinePaint.setColor(Color.WHITE);
+            mOnlinePaint.setShadowLayer(5.0f, 0.0f, 0.0f, Color.BLACK);
+
+            mAscent = mOnlinePaint.ascent();
+            float descent = mOnlinePaint.descent();
+            mFH = (int)(descent - mAscent + .5f);
+
+            updateDisplay();
+        }
+
+        @Override
+        protected void onAttachedToWindow() {
+            super.onAttachedToWindow();
+        }
+
+        @Override
+        protected void onDetachedFromWindow() {
+            super.onDetachedFromWindow();
+            mCurFPSHandler.removeMessages(1);
+        }
+
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            setMeasuredDimension(resolveSize(mNeededWidth, widthMeasureSpec),
+                    resolveSize(mNeededHeight, heightMeasureSpec));
+        }
+
+        private String getFPSInfoString() {
+            return mFps;
+        }
+
+        @Override
+        public void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            if (!mDataAvail) {
+                return;
+            }
+
+            final int W = mNeededWidth;
+            final int RIGHT = getWidth()-1;
+
+            int x = RIGHT - mPaddingLeft;
+            int top = mPaddingTop + 2;
+            int bottom = mPaddingTop + mFH - 2;
+
+            int y = mPaddingTop - (int)mAscent;
+
+            String s=getFPSInfoString();
+            canvas.drawText(s, RIGHT-mPaddingLeft-mMaxWidth,
+                    y-1, mOnlinePaint);
+            y += mFH;
+        }
+
+        void updateDisplay() {
+            if (!mDataAvail) {
+                return;
+            }
+
+            if (mOnlinePaint != null) {
+                mMaxWidth = (int) mOnlinePaint.measureText(mFps);
+            }
+
+            int neededWidth = mPaddingLeft + mPaddingRight + mMaxWidth;
+            int neededHeight = mPaddingTop + mPaddingBottom + 40;
+            if (neededWidth != mNeededWidth || neededHeight != mNeededHeight) {
+                mNeededWidth = neededWidth;
+                mNeededHeight = neededHeight;
+                requestLayout();
+            } else {
+                invalidate();
+            }
+        }
+
+        public Handler getHandler(){
+            return mCurFPSHandler;
+        }
+    }
+
+    protected class CurFPSThread extends Thread {
+        private boolean mInterrupt = false;
+        private Handler mHandler;
+
+        public CurFPSThread(Handler handler){
+            mHandler=handler;
+        }
+
+        public void interrupt() {
+            mInterrupt = true;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (!mInterrupt) {
+                    sleep(1000);
+                    StringBuffer sb=new StringBuffer();
+                    String fpsVal = FileUtils.readOneLine(MEASURED_FPS);
+                    mHandler.sendMessage(mHandler.obtainMessage(1, fpsVal));
+                }
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mView = new FPSView(this);
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.TYPE_SECURE_SYSTEM_OVERLAY,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+            PixelFormat.TRANSLUCENT);
+        params.y = 50;
+        params.gravity = Gravity.RIGHT | Gravity.TOP;
+        params.setTitle("FPS Info");
+
+        startThread();
+
+        mDreamManager = IDreamManager.Stub.asInterface(
+                ServiceManager.checkService(DreamService.DREAM_SERVICE));
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+
+        WindowManager wm = (WindowManager)getSystemService(WINDOW_SERVICE);
+        wm.addView(mView, params);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopThread();
+        ((WindowManager)getSystemService(WINDOW_SERVICE)).removeView(mView);
+        mView = null;
+        unregisterReceiver(mScreenStateReceiver);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                Log.d(TAG, "ACTION_SCREEN_ON " + isDozeMode());
+                if (!isDozeMode()) {
+                    startThread();
+                    mView.setVisibility(View.VISIBLE);
+                }
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                Log.d(TAG, "ACTION_SCREEN_OFF");
+                mView.setVisibility(View.GONE);
+                stopThread();
+            }
+        }
+    };
+
+    private boolean isDozeMode() {
+        try {
+            if (mDreamManager != null && mDreamManager.isDreaming()) {
+                return true;
+            }
+        } catch (RemoteException e) {
+            return false;
+        }
+        return false;
+    }
+
+    private void startThread() {
+        Log.d(TAG, "started CurFPSThread");
+        mCurFPSThread = new CurFPSThread(mView.getHandler());
+        mCurFPSThread.start();
+    }
+
+    private void stopThread() {
+        if (mCurFPSThread != null && mCurFPSThread.isAlive()) {
+            Log.d(TAG, "stopping CurFPSThread");
+            mCurFPSThread.interrupt();
+            try {
+                mCurFPSThread.join();
+            } catch (InterruptedException e) {
+            }
+        }
+        mCurFPSThread = null;
+    }
+}